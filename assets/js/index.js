2dad93ccf3382086409b9c73bc1b947643475012
rasouljabbari <rasoul_jabbari_1377@yahoo.com>
2021-09-21 18:03:40 +0430

    end product info page

--- assets/js/index.js
+++ assets/js/index.js
@@ -81,3 +81,70 @@ $('.panel-collapse').on('show.bs.collapse', function () {
 $('.panel-collapse').on('hide.bs.collapse', function () {
     $(this).siblings('.panel-heading').removeClass('active');
 });
+
+var miniSlider = new Swiper(".miniSlider", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var largeSlider = new Swiper(".largeSlider", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: miniSlider,
+    },
+});
+
+$.fn.digits = function(){
+    return this.each(function(){
+        $(this).text( $(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") );
+    })
+}
+
+$(document).ready(function () {
+    let price = $('.js-variant-selector').data('price')
+    let discount_price = $('.js-variant-selector').data('discount-price')
+    $('.dv-discount-price-value').text(discount_price)
+    $('.dv-discount-price-value').data('discount-unit-price', discount_price)
+    $('.dv-price-value').text(price)
+    $('.dv-price-value').data('unit-price' , price)
+    $(".dv-discount-price-value , .dv-price-value").digits();
+})
+
+$('.js-variant-selector').on('change' , function () {
+    let value = $(this).val()
+    let price = $(this).data('price')
+    let discount_price = $(this).data('discount-price')
+    $('.dv-color-name').text(value)
+    $('.dv-discount-price-value').text(discount_price)
+    $('.dv-price-value').text(price)
+
+    $('.dv-discount-price-value').data('discount-unit-price', discount_price)
+    $('.dv-price-value').data('unit-price' , price)
+    $('.dv-product-count').text(1)
+    $(".dv-discount-price-value , .dv-price-value").digits();
+})
+
+$('.fa-number-plus').on('click' , function () {
+    let count = $('.dv-product-count').text()
+    let price_unit = $('.dv-price-value').data('unit-price')
+
+    $('.dv-product-count').text(parseInt(count) + 1)
+    $('.dv-price-value').text((parseInt(count) + 1) * parseInt(price_unit))
+    $(".dv-discount-price-value , .dv-price-value").digits();
+})
+
+$('.fa-number-minus').on('click' , function () {
+    let count = $('.dv-product-count').text()
+    let price_unit = $('.dv-price-value').data('unit-price')
+
+    if(count > 1){
+        $('.dv-product-count').text(parseInt(count) - 1)
+        $('.dv-price-value').text((parseInt(count) - 1) * parseInt(price_unit))
+        $(".dv-discount-price-value , .dv-price-value").digits();
+    }
+})

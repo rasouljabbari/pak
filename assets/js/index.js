8a66cd508e9e6804e6347b06f21cb83581ec53e4
Farzan <farzaneyousefi788@gmail.com>
2021-09-23 17:09:28 +0330

    account , new password , confrim code ,

--- assets/js/index.js
+++ assets/js/index.js
@@ -193,3 +193,30 @@ $('input[name="payment_method"]').on('change', function (e) {
         $('.dv-online').addClass('d-none')
     }
 })
+
+const inputElements = [...document.querySelectorAll('input.code-input')]
+
+inputElements.forEach((ele,index)=>{
+    ele.addEventListener('keydown',(e)=>{
+        if(e.keyCode === 8 && e.target.value==='') inputElements[Math.max(0,index-1)].focus()
+    })
+    ele.addEventListener('input',(e)=>{
+        const [first,...rest] = e.target.value
+        e.target.value = first ?? ''
+        if(index!==inputElements.length-1 && first!==undefined) {
+            inputElements[index+1].focus()
+            inputElements[index+1].value = rest.join('')
+            inputElements[index+1].dispatchEvent(new Event('input'))
+        }
+    })
+})
+
+
+function onSubmit(e){
+    e.preventDefault()
+    const code = [...document.getElementsByTagName('input')]
+        .filter(({name})=>name)
+        .map(({value})=>value)
+        .join('')
+    console.log(code)
+}
